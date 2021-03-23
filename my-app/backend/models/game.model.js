d02d9e016233803125675d081a5b437bbe16e323
DiiZyy <zhangdavid33@gmail.com>
2021-03-23 17:59:22 -0400

    set up MongoDB and endpoint for adding pair

---
+++ my-app/backend/models/game.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const playerSchema = new Schema({
+    name: {
+      type: String,
+    },
+    id: {
+      type: Number,
+    },
+    score: {
+      type: Number,
+      default: 0,
+    },
+});
+
+const pairSchema = new Schema({
+    playerName: {
+        type: String,
+    },
+    playerId: {
+        type: Number,
+    },
+    songName: {
+        type: String,
+    },
+    songId: {
+        type: Number,
+    },
+});
+
+const gameSchema = new Schema({
+  joinCode: { type: String, unique: true},
+  players: [playerSchema],
+  songPlayerPairs: [pairSchema],
+}, {
+  timestamps: true,
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = Game;
