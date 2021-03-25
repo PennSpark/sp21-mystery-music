73cb6df608d7c6698c14c5da675aa06c0233ce42
Egret <egret@seas.upenn.edu>
2021-03-25 19:37:18 -0400

    Implement basic homepage for joining/creating games

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,232 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { Component } from 'react';
+import PubNubReact from 'pubnub-react';
+import Swal from "sweetalert2";
+import shortid from 'shortid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.pubnub = new PubNubReact({
+      publishKey: "pub-c-353f0377-1a09-4a4f-be96-1f51d601592c",
+      subscribeKey: "sub-c-7cb8e092-8c1a-11eb-9de7-3a1dcc291cdf"
+    });
+    this.state = {
+      player: '', // 1, 2, 3, 4, 5
+      isPlaying: false, // Set to true when at least 2 players are in a channel
+      isRoomCreator: false,
+      isDisabled: false,
+      myTurn: false,
+    };
+    this.lobbyChannel = null; // Lobby channel
+    this.gameChannel = null; // Game channel
+    this.size = null; // Number of players in room
+    this.roomId = null; // Unique id when player creates a room
+    this.pubnub.init(this); // Initialize PubNub
+  }
+
+  componentWillUnmount() {
+    this.pubnub.unsubscribe({
+      channels: [this.lobbyChannel, this.gameChannel]
+    });
+  }
+
+  componentDidUpdate() {
+    // Check that the player is connected to a channel
+    if (this.lobbyChannel != null) {
+      this.pubnub.getMessage(this.lobbyChannel, (msg) => {
+        // Start the game once an opponent joins the channel
+        if (msg.message.notRoomCreator) {
+          // Create a different channel for the game
+          this.gameChannel = 'spotifygame--' + this.roomId;
+
+          this.pubnub.subscribe({
+            channels: [this.gameChannel]
+          });
+
+          this.setState({
+            isPlaying: true
+          });
+
+          // Close the modals if they are opened
+          Swal.close();
+        }
+      });
+    }
+  }
+
+  // Create a room channel
+  onPressCreate = (e) => {
+    this.size = 1;
+    // Create a random name for the channel
+    this.roomId = shortid.generate().substring(0, 5);
+    this.lobbyChannel = 'spotifylobby--' + this.roomId;
+
+    this.pubnub.subscribe({
+      channels: [this.lobbyChannel],
+      withPresence: true
+    });
+
+
+
+    // Open the modal sweetalert2
+    Swal.mixin({
+      confirmButtonText: 'Next &rarr;',
+      showCancelButton: true,
+      progressSteps: ['1', '2']
+    }).queue([
+      {
+        title: 'Share this room ID with your friend',
+        text: this.roomId
+      },
+      {
+        title: 'Type your spotify here',
+        input: 'text'
+      }
+    ]).then((result) => {
+      if (result.value) {
+        // not sure if need to stringify with only one input, can't test ):
+        const answers = JSON.stringify(result.value)
+        Swal.fire({
+          html: `
+            Your spotify:
+            <pre><code>${answers}</code></pre>
+          `,
+          confirmButtonText: 'Play!'
+        })
+      }
+    })
+
+    this.setState({
+      player: '1',
+      isRoomCreator: true,
+      isDisabled: true, // Disable the 'Create' button
+      myTurn: true, // Room creator makes the 1st move
+    });
+  }
+
+  // The 'Join' button was pressed
+  onPressJoin = (e) => {
+
+    Swal.mixin({
+      confirmButtonText: 'Next &rarr;',
+      showCancelButton: true,
+      progressSteps: ['1', '2']
+    }).queue([
+      {
+        inputPlaceholder: 'Enter the room id',
+        input: 'text'
+      },
+      {
+        title: 'Type your spotify here',
+        input: 'text'
+      }
+    ]).then((result) => {
+      if (result.value) {
+        const answers = JSON.stringify(result.value)
+        Swal.fire({
+          html: `
+            Your spotify:
+            <pre><code>${answers[0]}</code></pre>
+          `,
+          confirmButtonText: 'Play!'
+        })
+        // Check if the user typed a value in the input field
+        if (result.value[1]) {
+          this.joinRoom(result.value[1]);
+        }
+      }
+    })
+
+  }
+
+  // Join a room channel
+  joinRoom = (value) => {
+    this.roomId = value;
+    this.lobbyChannel = 'spotifylobby--' + this.roomId;
+
+    // Check the number of people in the channel
+    this.pubnub.hereNow({
+      channels: [this.lobbyChannel],
+    }).then((response) => {
+      if (response.totalOccupancy < 5) {
+        this.pubnub.subscribe({
+          channels: [this.lobbyChannel],
+          withPresence: true
+        });
+
+        this.setState({
+          player: `${response.totalOccupancy + 1}`,
+        });
+
+        this.pubnub.publish({
+          message: {
+            notRoomCreator: true,
+          },
+          channel: this.lobbyChannel
+        });
+      }
+      else {
+        // Game in progress
+        Swal.fire({
+          title: 'Error',
+          text: 'Game is full.',
+        })
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  // Reset everything
+  endGame = () => {
+    this.setState({
+      player: '',
+      isPlaying: false,
+      isRoomCreator: false,
+      isDisabled: false,
+      myTurn: false,
+    });
+
+    this.lobbyChannel = null;
+    this.gameChannel = null;
+    this.roomId = null;
+
+    this.pubnub.unsubscribe({
+      channels: [this.lobbyChannel, this.gameChannel]
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="title">
+          <p> Spotify Game </p>
+        </div>
+        {
+          !this.state.isPlaying &&
+          <div className="game">
+            <div className="board">
+              <div className="button-container">
+                <button
+                  className="create-button "
+                  disabled={this.state.isDisabled}
+                  onClick={(e) => this.onPressCreate()}
+                > Create
+                </button>
+                <button
+                  className="join-button"
+                  onClick={(e) => this.onPressJoin()}
+                > Join
+                </button>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
+    );
+  }
 }
-
 export default App;
